3eb10c4462944e029de84fe586b4acf70804f92f
Matthew <plummerfernandez@gmail.com>
2014-11-18 17:50:32 +0100

    Update minecraftbot.py

--- minecraftbot.py
+++ minecraftbot.py
@@ -1,6 +1,13 @@
-import os
-
-os.system("echo 'hello world'")
-os.system("minecraft-pi")
-
 # then launch from SSH as 'sudo python minecraftbot.py'
+import os
+from mcpi import minecraft
+
+## Sadly this hack to launch minecraft via SSH didn't work. 
+#os.system("echo 'hello world'")
+#os.system("minecraft-pi")  
+
+## So we have to use a program like Remoter to look at the rPi screen remotely and click to launch Minecraft
+
+## now we can initiate our python - minecraft session
+mc = minecraft.Minecraft.create()
+mc.postToChat("Hello world")

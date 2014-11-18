14f4fcf7a3a0d75e1e9935be8b5e220046785404
Matthew <plummerfernandez@gmail.com>
2014-11-18 16:26:27 +0100

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -1,11 +1,33 @@
-#A bot to check the weather using the python weather API
-# https://code.google.com/p/python-weather-api/
+# a basic weather-checking bot example for the Botcaves workshop
 
 import pywapi
 import pprint
+import string
+from threading import Event, Thread, Timer
+
 pp = pprint.PrettyPrinter(indent=4)
 
-result = pywapi.get_weather_from_yahoo('RSXX0199', 'metric')
-pp.pprint(result)
 
-temperature = weather_com_result['current_conditions']['temperature']
+# method for checking the weather
+def checkWeather():
+	weather_com_result = pywapi.get_weather_from_weather_com('10001')
+	#pp.pprint(weather_com_result)
+	temperature = weather_com_result['current_conditions']['temperature']
+	print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
+
+
+## METHOD TO RUN AT INTERVALS (NODE STYLE)
+def call_repeatedly(interval, func, *args): ## you can pass arguments too which is useful
+    stopped = Event()
+    def loop():
+        while not stopped.wait(interval): # the first call is in `interval` secs
+            func(*args)
+    Thread(target=loop).start()    
+    return stopped.set
+    
+
+# check the weather once at startup
+checkWeather()
+
+# check the weather every 20 seconds
+check_weather_timer = call_repeatedly(20, checkWeather) #seconds

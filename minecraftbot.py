cef1422d3cd4b890e8b366f47519916c2745ff80
Matthew <plummerfernandez@gmail.com>
2014-11-19 09:59:51 +0100

    Update minecraftbot.py

--- minecraftbot.py
+++ minecraftbot.py
@@ -1,18 +1,30 @@
-# then launch from SSH as 'sudo python minecraftbot.py'
-import os
+#first launch minecraft manually
+
 from mcpi import minecraft
-
-## Sadly this hack to launch minecraft via SSH didn't work. 
-#os.system("echo 'hello world'")
-#os.system("minecraft-pi")  
-
-## So we have to use a program like Remoter to look at the rPi screen remotely and click to launch Minecraft
-
-## now we can initiate our python - minecraft session
+from threading import Event, Thread, Timer
 mc = minecraft.Minecraft.create()
-mc.postToChat("Hello world")
 
+
+# Recursively set block
+def setblock():
+	mc.postToChat("adding block")
+	x, y, z = mc.player.getPos()
+	mc.setBlock(x+1, y, z, 1)
+	mc.player.setPos(x-1,y,z)
+	
+## METHOD TO RUN AT INTERVALS (NODE STYLE)
+def call_repeatedly(interval, func, *args): ## you can pass arguments too which is useful
+    stopped = Event()
+    def loop():
+        while not stopped.wait(interval): # the first call is in `interval` secs
+            func(*args)
+    Thread(target=loop).start()    
+    return stopped.set
+    
+
+setblock_timer = call_repeatedly(5, setblock) #seconds
 
 ## for more try these links:
+# http://www.stuffaboutcode.com/2013/01/raspberry-pi-minecraft-api-basics.html
 # http://www.raspberrypi.org/documentation/usage/minecraft/
 # http://www.stuffaboutcode.com/p/minecraft.html
